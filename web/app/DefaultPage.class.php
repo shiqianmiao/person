f70c8a0bb843934d3494578001b6894fe4cead4c
miaoshiqian <miaoshiqian@miaoshiqian-N73SV.(none)>
2014-07-04 11:02:38 +0800

    submit resume page!

--- web/app/DefaultPage.class.php
+++ web/app/DefaultPage.class.php
@@ -10,13 +10,27 @@ require_once WEB_V3 . '/common/WebBasePage.class.php' ;
  * @desc      
  */
 class DefaultPage extends WebBasePage{
-    
+    /**
+     *@desc home Page
+     */
     function defaultAction() {
 
         $staHtml .= 'g.js,config.js,app/person/web/css/index_v2.css';
         $staHtml = $this->view->helper('sta', array('files' => $staHtml));
 
         $this->render(array('staHtml' => $staHtml), 'default.php');
+
+    }
+
+    /**
+     *@desc resume Page
+     */
+    function resumeAction() {
+
+        $staHtml .= 'g.js,config.js,app/person/web/css/index_v2.css';
+        $staHtml = $this->view->helper('sta', array('files' => $staHtml));
+
+        $this->render(array('staHtml' => $staHtml), 'resume.php');
 
     }
     
--- web/template/resume.php
+++ web/template/resume.php
@@ -42,7 +42,7 @@
             
         <ul>
             <li><a href="http://www.miaosq.com" class="current">家园主页</a></li>
-            <li><a href="/resume/">关于园主</a></li>
+            <li><a href="about.html">关于园主</a></li>
             <li><a href="services.html">WEB记事本</a></li>
             <li><a href="gallery.html">相册</a></li>
             <li class="last"><a href="contact.html">网站日志</a></li>
@@ -52,51 +52,6 @@
     
     <div id="content_wrapper">
       
-        <div id="content">
-
-        <h2>文章摘要简介</h2>
-            
-            <p><em><a href="#">Total 5 pages</a> included in this template. Feel free to download, adapt, and apply this template for any purpose. Maecenas nisl ante, mollis et, tincidunt vitae, feugiat sit amet, mi.   Vestibulum urna. Vivamus eu tortor. Phasellus tempus, justo et laoreet   varius, odio mi ultrices libero, nec faucibus dui felis eu dui. Cras ac   odio ac mi imperdiet sollicitudin.</em> </p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href="#">Praesent volutpat</a> dolor at nulla egestas in vestibulum mauris lacinia. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. <a href="#">Sed pretium</a>, neque hendrerit rhoncus accumsan, nibh tellus pharetra   neque, quis rutrum elit justo vitae sapien.</p>
-        <ul class="templatemo_list col_w260 float_l">
-              <li>Aliquam pretium porta odio</li>
-                <li>Praesent sollicitudin mollis leo</li>
-                <li>Quisque venenatis fringilla eros</li>
-            </ul> 
-    <ul class="templatemo_list col_w260 float_r">
-                <li>Mauris vel sapien mattis urna luctus</li>
-                <li>Vivamus fermentum orci viverra nisl</li>
-                <li>Curabitur euismod scelerisque lacus</li>
-            </ul>  
-          <div class="cleaner"></div>
-            <div class="button float_r"><a href="services.html">Read More...</a></div>
-            
-            <div class="cleaner_h20"></div>
-            
-             <h2>Website Simples</h2>
-             
-             <p>Etiam in lorem ac nulla imperdiet sodales. Phasellus in justo at lectus pulvinar ultrices. Mauris ultrices sollicitudin turpis. Fusce non lacus et lorem ornare interdum. Nunc eget urna. Mauris vestibulum felis eget tortor. Praesent mattis varius quam.</p>
-             
-<div id="gallery_home">
-                <ul>
-                
-                    <li>
-                        <a href="http://sta.miaosq.com/app/person/web/img/gallery/image_01_b.jpg" class="pirobox" title="Project 1"><img src="http://sta.miaosq.com/app/person/web/img/gallery/image_01.jpg" alt="" /></a>
-                    </li>
-                    <li>
-                        <a href="http://sta.miaosq.com/app/person/web/img/gallery/image_02_b.jpg" class="pirobox" title="Project 1"><img src="http://sta.miaosq.com/app/person/web/img/gallery/image_02.jpg" alt="" /></a>
-                    </li>
-                    <li class="last">
-                        <a href="http://sta.miaosq.com/app/person/web/img/gallery/image_03_b.jpg" class="pirobox" title="Project 1"><img src="http://sta.miaosq.com/app/person/web/img/gallery/image_03.jpg" alt="" /></a>
-                    </li>
-                    
-                    </ul>
-                </div>
-            
-                <div class="cleaner_h30"></div>
-            <div class="button float_r"><a href="gallery.html">View All</a></div>
-            
-      </div>
         
         <div id="sidebar_wrapper">
         
