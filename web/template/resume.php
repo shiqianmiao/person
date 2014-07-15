8f0e6cfb6d7e5fc73a36f3386b9693b6a541a7ad
miaoshiqian <miaoshiqian@miaoshiqian-N73SV.(none)>
2014-07-15 17:30:24 +0800

    submit resume page

--- web/template/resume.php
+++ web/template/resume.php
@@ -1,117 +1,97 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Shiqianmiao's Home Page!</title>
-<meta name="keywords" content="个人网站,miaoshiqian, shiqianmiao, 缪石乾" />
-<meta name="description" content="缪石乾的个人网站" />
-<?php echo $this->staHtml;?>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Shiqianmiao's Home Page!</title>
+    <meta name="keywords" content="个人网站,miaoshiqian, shiqianmiao, 缪石乾" />
+    <meta name="description" content="缪石乾的个人网站" />
+    <?php echo $this->staHtml;?>
 
+    <style type="text/css">
+        #content_wrapper table {
+            border:solid 3px white;
+        }
+    </style>
 </head>
+
+
 <body> 
-
-<div id="templatemo_wrapper">
-  
+<div id="templatemo_wrapper"> 
     <div id="templatemo_header">
-
-        <div id="header_left">
-            
+        <div id="header_left">   
             <div id="site_title">
                 <a href="http://www.miaosq.com/" target="_parent"><img src="http://sta.miaosq.com/app/person/web/img/templatemo_logo.png" alt="logo" /><span>Miao Shiqian's Home!!</span></a>
             </div>
-    
         </div>
         
-        <div id="header_right">
-        
+        <div id="header_right"> 
           <ul id="social_box">
               <li><a href="#"><img src="http://sta.miaosq.com/app/person/web/img/facebook.png" alt="facebook" /></a></li>
                 <li><a href="#"><img src="http://sta.miaosq.com/app/person/web/img/twitter.png" alt="twitter" /></a></li>
                 <li><a href="#"><img src="http://sta.miaosq.com/app/person/web/img/linkedin.png" alt="linkin" /></a></li>
                 <li><a href="#"><img src="http://sta.miaosq.com/app/person/web/img/technorati.png" alt="technorati" /></a></li>
                 <li><a href="#"><img src="http://sta.miaosq.com/app/person/web/img/myspace.png" alt="myspace" /></a></li>                
-            </ul>
-            
+            </ul>  
         </div>
     
-      <div class="cleaner"></div>
-        
+        <div class="cleaner"></div>  
     </div>
     
-    <div id="templatemo_menu">
-            
+    <div id="templatemo_menu">   
         <ul>
             <li><a href="http://www.miaosq.com" class="current">家园主页</a></li>
-            <li><a href="about.html">关于园主</a></li>
+            <li><a href="http://www.miaosq.com/resume/">关于园主</a></li>
             <li><a href="services.html">WEB记事本</a></li>
             <li><a href="gallery.html">相册</a></li>
             <li class="last"><a href="contact.html">网站日志</a></li>
         </ul>     
+    </div>
     
-    </div> <!-- end of templatemo_menu -->
-    
-    <div id="content_wrapper">
-      
-        
-        <div id="sidebar_wrapper">
-        
-          <div id="sidebar_top"></div>
-            
-            <div id="sidebar">
-           
-                <h2>Latest News</h2>
-                
-                   <div class="sb_news_box">
-                        
-                        <img src="http://sta.miaosq.com/app/person/web/img/templatemo_image_01.jpg" alt="image" />
-                        <div class="nb_right">
-                            <a href="#">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit dictumst.</a>
-                        </div>
-                        <div class="cleaner"></div>
-                        
-                    </div>
-                    
-                    <div class="sb_news_box">
-                        
-                        <img src="http://sta.miaosq.com/app/person/web/img/templatemo_image_02.jpg" alt="image" />
-                        <div class="nb_right">
-                            <a href="#">
-                            Praesent sed est urna, id adipiscing nunc. Etiam laoreet ante quis.</a>
-                        </div>
-                        <div class="cleaner"></div>
-                        
-                    </div>
-                    
-                    <div class="sb_news_box">
-                        
-                        <img src="http://sta.miaosq.com/app/person/web/img/templatemo_image_03.jpg" alt="image" />
-                        <div class="nb_right">
-                            <a href="#">
-                            Cras bibendum, mi vitae pharetra tincidunt sed ullamcorper mauris</a>
-                        </div>
-                        <div class="cleaner"></div>
-                        
-                    </div>
-                    
-                    <div class="button float_r"><a href="#">Read More...</a></div>
-                
-                    <div class="cleaner_h30"></div>
+    <div id="content_wrapper"> 
+        <div id="content">
 
-      <h2>Categories</h2>
-
-      <ul class="templatemo_list col_w260">
-              <li><a href="#">Vestibulum ullamcorper</a></li>
-                <li><a href="#">Praesent sollicitudin mollis</a></li>
-                <li><a href="#">Etiam in lorem ac nulla</a></li>
-                <li><a href="#">Quisque venenatis fringilla</a></li>
-            </ul>
+        <table>
+            <tr><td><h2>园主简历</h2></td><td></td></tr>
+            <tr>
+                <td><span class="image_wrapper"><img src="http://sta.miaosq.com/app/person/web/img/templatemo_image_04.jpg" alt="image" /></span></td>
+                <td></td>
+            </tr>
+        </table>
+         <p><em>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent aliquam velit a magna sodales quis elementum ipsum auctor. Ut at metus leo, et dictum sem</em></p>
+        <p>In ac libero urna. Suspendisse sed odio ut mi auctor blandit. Duis luctus nulla metus, a vulputate mauris. Integer sed nisi sapien, ut gravida mauris. Nam et tellus libero. Cras purus libero, dapibus nec rutrum in, dapibus nec risus. <a href="#">Ut interdum mi</a> sit amet magna feugiat auctor. </p>
+        <div class="image_wrapper fr_img"><img src="images/templatemo_image_05.jpg" alt="image" /></div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat dolor at nulla egestas in vestibulum mauris lacinia. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut sed accumsan nisl. Donec malesuada augue ac nisl sagittis quis cursus dolor scelerisque. </p>
+        <p><a href="#">Aliquam ac massa ut</a> nibh pulvinar aliquam vel id metus. Nulla id ante a diam volutpat commodo. Curabitur vitae enim sit amet erat ultricies hendrerit. Sed metus leo, condimentum ut ornare sit amet, congue et mauris. Phasellus mattis posuere magna sit amet consectetur.</p>
+        <div class="cleaner_h40"></div>
+        
+        <div class="col_w260 float_l">
+        
+          <h2>Services</h2>
+            <p>In ac libero urna. Suspendisse sed odio ut mi auctor blandit. Duis luctus nulla metus, a vulputate mauris.</p>
             
-            </div>
-            
-            <div id="sidebar_bottom"></div>
-            
+<ul class="templatemo_list">
+                    <li>Est cursus suscipit eu ac lectus.</li>
+                    <li>Nibh nisi, sed eleifend dolor. </li>
+                    <li>Sollicitudin sapien nec aliquet. </li>
+                    <li>Cras rutrum  consectetur dolor </li>
+                </ul>
+        
         </div>
+        
+        <div class="col_w260 float_r">
+        
+            <h2>Testimonial</h2>
+            
+            <blockquote>
+            <p>Fusce nec felis id lacus sollicitudin vulputate. Proin tincidunt, arcu id pellentesque accumsan, neque dolor imperdiet ligula, quis viverra tellus nulla a odio. Curabitur vitae enim risus, at placerat turpis. Mauris feugiat suscipit tempus fringilla, felis in velit.</p>
+            
+            <cite>Steven Wood - <span>Web Designer</span></cite>
+            </blockquote>
+            
+        
+        </div>
+            
+      </div>
         
         <div class="cleaner"></div>
     
